782038b5d2f247757cce006ab1d2f05e525de9bc
Andrei Boghean <andreiboghean00+github@gmail.com>
2025-02-05 22:47:19 +0000

    add initial and basic glime support

---
+++ installGlime.sh
@@ -0,0 +1,35 @@
+#! /bin/bash -vx
+
+# args :
+# (1) wasp-os directory
+# (2) glime repo download location.
+
+wasp=$1
+glime=$2
+
+if [ -f $wasp ]; then wasp="wasp-os"; fi
+if [ -f $glime ]; then glime="glime"; fi
+
+wasp=$(readlink $wasp -f)
+glime=$(readlink $glime -f)
+
+echo "wasp: $wasp"
+echo "glime: $glime"
+
+# "install" process
+
+# insert the wrapper app which acts as the "barrier" between OS and external app.
+# the wrapper app calls the main() of the external app.
+ln -v -s $(readlink $glime/wasp-os_srcs/wrapApp.py -f) $wasp/apps/wrap.py
+
+mkdir -p $wasp/modules/crossApp
+
+# insert the compatability interface and wasp-os specific implementation as a C-implemented micropython module.
+ln -v -s $(readlink $glime/common_srcs/compat.h -f) $wasp/modules/crossApp/compat.h
+ln -v -s $(readlink $glime/common_srcs/compat.py.cpp -f) $wasp/modules/crossApp/compat.py.cpp
+# also insert the actual OS-agnostic external app
+ln -v -s $(readlink $glime/common_srcs/externApp.h -f) $wasp/modules/crossApp/externApp.h
+ln -v -s $(readlink $glime/common_srcs/externApp.cpp -f) $wasp/modules/crossApp/externApp.cpp
+# lastly add the module config files that define the bindings between C and micropython.
+ln -v -s $(readlink $glime/wasp-os_srcs/internal_module/gateway.c -f) $wasp/modules/crossApp/gateway.c
+ln -v -s $(readlink $glime/wasp-os_srcs/internal_module/micropython.mk -f) $wasp/modules/crossApp/micropython.mk
