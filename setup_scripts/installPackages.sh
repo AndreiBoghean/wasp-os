a07aac09f08924bc6129ba3e2c7af40bd340d971
AndreiBoghean <67372068+AndreiBoghean@users.noreply.github.com>
2025-02-09 13:21:06 +0000

    Create installPackages.sh

---
+++ setup_scripts/installPackages.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cd $(dirname $(readlink -f $0))/.. # move into wasp-os folder, relative to this script's location in wasp-os/setup_scripts
+# ^ needed since pip below uses a relative link for reqs.txt
+
+sudo pacman -Sy base-devel unzip
+pip install --break-system-packages -r wasp/requirements.txt
+pip install --break-system-packages -r tools/nrfutil/requirements.txt
