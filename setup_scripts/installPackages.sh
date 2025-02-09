2710316299de31dde86baeb329904a1a14afc26a
Andrei Boghean <andreiboghean00+github@gmail.com>
2025-02-09 13:42:23 +0000

    add make submodules to installPackages.sh so nrfutils/requirements.txt is available

--- setup_scripts/installPackages.sh
+++ setup_scripts/installPackages.sh
@@ -4,5 +4,7 @@ cd $(dirname $(readlink -f $0))/.. # move into wasp-os folder, relative to this 
 # ^ needed since pip below uses a relative link for reqs.txt
 
 sudo pacman -Sy base-devel unzip
+
+make submodules # needed for tools/nrfutil
 pip install --break-system-packages -r wasp/requirements.txt
 pip install --break-system-packages -r tools/nrfutil/requirements.txt
